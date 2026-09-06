edfa029d947f818a624b3228f07665cb95208d96
Jayavardhan456 <n231035@rgukt.ac.in>
2026-09-06 19:37:07 +0530

    Fixed

--- backend/main.py
+++ backend/main.py
@@ -35,12 +35,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://resolve-ai-rust.vercel.app",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
